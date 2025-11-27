2fc3940576440de790df618b73d717dda1a3a761
eroccked <eroccked@gmail.com>
2025-11-27 08:15:00 +0200

    Initial Commit

---
+++ PowerScheduleIos/PowerScheduleIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerScheduleIosApp.swift
+//  PowerScheduleIos
+//
+//  Created by Taras Buhra on 27.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct PowerScheduleIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
